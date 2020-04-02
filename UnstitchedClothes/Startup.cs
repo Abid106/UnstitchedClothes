cd602b421134d019256164306bc966dd53ebaee3
Abid <logicsinn1@outlook.com>
2020-04-02 19:57:17 +0500

    Add project files.

---
+++ UnstitchedClothes/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UnstitchedClothes.Startup))]
+namespace UnstitchedClothes
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
